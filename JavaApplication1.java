d26ef361100a74b0ae3a02c447e4e78cac67826e
grimaldoccalla <48729500+grimaldoccalla@users.noreply.github.com>
2019-04-01 16:52:30 +0200

    Add files via upload

---
+++ JavaApplication1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author PC10-LAB02
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre ="Grimaldo";
+        String apellido="Ccalla";
+        System.out.println("concatenacion:"+nombre+apellido);
+        System.out.println("nueva linea:\n"+nombre+" "+apellido);
+        System.out.println("tabulador:\t"+nombre+apellido);
+        System.out.println("Retroceso:\b"+nombre+apellido);
+        System.out.println("retorno de carro:\r"+nombre+apellido);
+        System.out.println("Camilla Simple:'"+nombre+" "+apellido+"\'");
+        System.out.println("Comilla Doble:\""+nombre+" "+apellido+"\"");
+        
+        
+        
+        
+
+        // TODO code application logic here
+    }
+    
+}
