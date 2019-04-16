58ab3cfc633cc7ea3ae9ba6ea50b028afd273ecb
grimaldoccalla <48729500+grimaldoccalla@users.noreply.github.com>
2019-04-16 19:20:41 +0200

    Add files via upload

---
+++ DOWHILE.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgdo.pkgwhile;
+
+/**
+ *
+ * @author PC10-LAB02
+ */
+public class DOWHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int n=10;
+        int c=1;
+        do  {
+        System.out.println(c);
+        c++;
+        
+        }while(c<=n);
+        
+                
+        
+    }
+        // TODO code application logic here
+    }
+    
+}
