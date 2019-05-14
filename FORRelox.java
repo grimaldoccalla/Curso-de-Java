c0b92f95668f03886d1aa38d7e2bfae40236b9e0
grimaldoccalla <48729500+grimaldoccalla@users.noreply.github.com>
2019-05-14 19:11:13 +0200

    Add files via upload

---
+++ FORRelox.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgfor.relox;
+
+/**
+ *
+ * @author PC10-LAB02
+ */
+public class FORRelox {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int j=1;
+        int n=9;
+        for (int h=j ; h<=n;h++){
+            for(int m=00;m<=59;m++){
+                System.out.println(h+":"+m);
+                
+            }
+        }
+    }
+    
+}
